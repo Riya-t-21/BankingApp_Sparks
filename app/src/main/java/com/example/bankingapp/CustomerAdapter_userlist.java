fa6f40f314b9404d8b96088a30b040cf03a9034a
Riya-t-21 <102308368+Riya-t-21@users.noreply.github.com>
2023-01-20 03:02:18 +0530

    view list of users in progress

--- app/src/main/java/com/example/bankingapp/CustomerAdapter_userlist.java
+++ app/src/main/java/com/example/bankingapp/CustomerAdapter_userlist.java
@@ -1,5 +1,36 @@
 package com.example.bankingapp;
 
-public class CustomerAdapter_userlist
-{
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CustomerAdapter_userlist extends RecyclerView.Adapter {
+    user_list UserList;
+    List<Model> modelList;
+    Context context;
+
+    public CustomerAdapter_userlist(user_list userList, List<Model> modelList) {
+        this.UserList = userList;
+        this.modelList = modelList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
--- app/src/main/java/com/example/bankingapp/user_list.java
+++ app/src/main/java/com/example/bankingapp/user_list.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,12 +34,38 @@ public class user_list extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView_u.setLayoutManager(layoutManager);
 
+        showData();
     }
 
   private void showData()
   {
       modelList_showlist.clear();
-      //Cursor cursor = new DatabaseHelper(this);
+      Cursor cursor = new DatabaseHelper(this).readAlldata();
+      while(cursor.moveToNext())
+      {
+          String balancefromdb = cursor.getString(2);
+          Double balance = Double.parseDouble(balancefromdb);
+
+          NumberFormat nf = NumberFormat.getNumberInstance();
+          nf.setGroupingUsed(true);
+          nf.setMaximumFractionDigits(2);
+          nf.setMinimumFractionDigits(2);
+          String price = nf.format(balance);
+
+          Model model = new Model(cursor.getString(0), cursor.getString(1), price);
+          modelList_showlist.add(model);
+
+      }
+      adapter = new CustomerAdapter_userlist(user_list.this, modelList_showlist);
+      recyclerView_u.setAdapter(adapter);
+
   }
+    public void nextActivity(int position) {
+        phonenumber = modelList_showlist.get(position).getPhoneno();
+        Intent intent = new Intent(user_list.this, user_data.class);
+        intent.putExtra("phonenumber",phonenumber);
+        startActivity(intent);
+    }
+
 
 }
